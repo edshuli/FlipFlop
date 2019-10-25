ae9f795858c0853e1c6e9ad3aeb60c23dfa8096e
Ubuntu <ubuntu@ip-172-31-19-38.us-east-2.compute.internal>
2019-10-25 15:10:34 +0000

    Add travelInspiration.html and images and carousel on index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 $(".fa").mouseenter(function() {
-    $(this).addClass("changeColor");
+  $(this).addClass("changeColor");
 });
 
 $(".fa").mouseleave(function() {
-    $(this).removeClass("changeColor");
+  $(this).removeClass("changeColor");
 });
 
 $("h5").mouseenter(function() {
-    $(this).addClass("highLight");
+  $(this).addClass("highLight");
 });
 
 $("h5").mouseleave(function() {
-    $(this).removeClass("highLight");
+  $(this).removeClass("highLight");
 });
 
 //we use http://iamceege.github.io/tooltipster/ the tootipster site for this function
 $(document).ready(function() {
-    $('.tooltip').tooltipster();
+  $('.tooltip').tooltipster();
 });
 
 //Took this from stackoverflow
@@ -24,22 +24,48 @@ var myNav = $(".navbar");
 
 $(window).on('scroll', function() {
   "use strict";
-  if ($(window).scrollTop() >= 280) {
+  if ($(window).scrollTop() >= 550) {
     myNav.addClass("scroll");
-  } else {
+  }
+  else {
     myNav.removeClass("scroll");
   }
 });
 
 
 $("input").mouseenter(function() {
-    $(".searchButton").addClass("borderColor");
+  $(".searchButton").addClass("borderColor");
 });
-
 $("input").mouseleave(function() {
-    $(".searchButton").removeClass("borderColor");
-})
-
-$("searchButton").click(function() {
-    $(".form-inline").addClass("borderColor");
+  $(".searchButton").removeClass("borderColor");
 });
+
+
+
+//function initMap() {
+//  var map = new google.maps.Map(document.getElementById("map"), {
+//    zoom: 3,
+//    center: {
+//      lat: 46.619261,
+//      lng: -33.134766
+//    }
+//  });
+//}
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.triposo.com/api/20190906/location.json?<ZNP76WBG>&token=<4vkqxzy5ob3vu3lqh617fzxkzygqwe>");
+
+xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+    };
+}
+
+setTimeout(function() {
+    console.log(data);
+}, 500);
+
+
